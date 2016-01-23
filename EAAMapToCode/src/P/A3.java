44a6cbd002cb0335abf0c57719c79b04d1ab84fd
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-01-24 00:03:51 +0700

    Add computational method

--- EAAMapToCode/src/P/A3.java
+++ EAAMapToCode/src/P/A3.java
@@ -10,5 +10,14 @@ public class A3 extends AMaster {
 	public A3() {
 		listOfB3 = new ArrayList<B3>();
 	}
+	
+	public int getSumNumberOfBElements() {
+		int totalNumberOfElements = 0;
+		
+		for(B3 b : listOfB3) {
+			totalNumberOfElements += b.getbVar1();
+		}
+		return totalNumberOfElements;
+	}
 
 }
