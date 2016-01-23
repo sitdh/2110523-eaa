43aa8d1e25d4a1e5c29b849a8beff02b5850c560
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-01-23 23:59:31 +0700

    Add compute elements

---
+++ EAAMapToCode/src/test/AssociationWithOneToManyRelationshipTests.java
@@ -0,0 +1,44 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import P.A3;
+import P.B3;
+
+public class AssociationWithOneToManyRelationshipTests {
+
+	@Test
+	public void testBShouldGetCorrectSum() {
+		B3 b3 = new B3Mock();
+		
+		assertEquals(10, b3.getbVar1());
+		assertEquals(20, b3.getbVar2());
+		
+		assertEquals(30, b3.getSummationOfVar1andVar2());
+	}
+	
+	public void testAShouldGetSumOfMultipleInstanceOfB() {
+		A3 a3 = new A3();
+		
+		for(int i = 0; i < 3; i++) {
+			B3 b3 = new B3Mock();
+			a3.listOfB3.add(b3);
+		}
+		
+		assertEquals(30, a3.getSumNumberOfBElements());
+	}
+	
+	public class B3Mock extends B3 {
+		
+		public int getbVar1() {
+			return 10;
+		}
+		
+		public int getbVar2() {
+			return 20;
+		}
+	}
+
+}
