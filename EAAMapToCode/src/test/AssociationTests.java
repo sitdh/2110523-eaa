db361fcd566364bf48180753dcda9724549bb235
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-01-23 23:11:55 +0700

    Add association's relation test case

---
+++ EAAMapToCode/src/test/AssociationTests.java
@@ -0,0 +1,31 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import P.A2;
+import P.B2;
+
+public class AssociationTests {
+
+	@Test
+	public void test() {
+		
+		A2 a2 = new A2();
+		a2.linkB2 = new B2Mock();
+		
+		assertEquals(20, a2.getSummationOfVar1andVar2());
+	}
+	
+	public class B2Mock extends B2 {
+		public int getbVar1() {
+			return 10;
+		}
+		
+		public int getbVar2() {
+			return 10;
+		}
+	}
+
+}
