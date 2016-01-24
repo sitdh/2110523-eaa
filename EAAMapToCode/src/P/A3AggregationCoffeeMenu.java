85012de7e3f6190dbcc397368d35f6ce373e22e2
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-01-24 23:35:23 +0700

    Add coffee and coffee's menu class

--- EAAMapToCode/src/P/A3AggregationCoffeeMenu.java
+++ EAAMapToCode/src/P/A3AggregationCoffeeMenu.java
@@ -1,5 +1,26 @@
 package P;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class A3AggregationCoffeeMenu {
+	
+	private List<B3AggregationCoffee> coffeeList;
+	
+	public A3AggregationCoffeeMenu() {
+		coffeeList = new ArrayList<B3AggregationCoffee>();
+	}
+	
+	public void addCoffee(B3AggregationCoffee coffee) {
+		coffeeList.add(coffee);
+	}
+	
+	public int getNumberOfCofeeInMenu() {
+		return coffeeList.size();
+	}
+	
+	public List<B3AggregationCoffee> getCoffee() {
+		return coffeeList;
+	}
 
 }
---
+++ EAAMapToCode/src/P/B3AggregationCoffee.java
@@ -0,0 +1,31 @@
+package P;
+
+public class B3AggregationCoffee {
+	
+	public String coffeeLabel;
+	public int price;
+	
+	public B3AggregationCoffee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getCoffeeLable() {
+		return coffeeLabel;
+	}
+	
+	public void setCoffeeLabel(String coffeeLabel) {
+		this.coffeeLabel = coffeeLabel;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+
+}
