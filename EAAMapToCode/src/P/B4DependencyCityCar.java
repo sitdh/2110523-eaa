5ce9383b2e534e7283d87d735c4afc48dc8eec42
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-01-25 13:54:33 +0700

    Car model

---
+++ EAAMapToCode/src/P/B4DependencyCityCar.java
@@ -0,0 +1,17 @@
+package P;
+
+public class B4DependencyCityCar implements B4DependencyCarForRent {
+
+	@Override
+	public String getCarBrand() {
+		// TODO Auto-generated method stub
+		return "City car";
+	}
+
+	@Override
+	public String getCarEngineName() {
+		// TODO Auto-generated method stub
+		return "APQ-5743";
+	}
+
+}
