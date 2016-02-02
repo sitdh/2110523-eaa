2c73b95d39c05bb1bf1d69d5c166a89a0b573328
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-02-02 20:19:13 +0700

    Add now class for 1st case

---
+++ EAAMapToCode/src/com/sitdh/master/eaa/main/A1Motorbike.java
@@ -0,0 +1,39 @@
+package com.sitdh.master.eaa.main;
+
+public class A1Motorbike {
+	
+	public int wheelSize;
+	
+	private float length;
+	
+	protected String blueprint;
+	
+	public A1Motorbike() {
+		
+	}
+
+	public int getWheelSize() {
+		return wheelSize;
+	}
+
+	public void setWheelSize(int wheelSize) {
+		this.wheelSize = wheelSize;
+	}
+
+	public float getLength() {
+		return length;
+	}
+
+	public void setLength(float length) {
+		this.length = length;
+	}
+
+	public String getBlueprint() {
+		return blueprint;
+	}
+
+	public void setBlueprint(String blueprint) {
+		this.blueprint = blueprint;
+	}
+
+}
---
+++ EAAMapToCode/src/com/sitdh/master/eaa/main/B1Helicopter.java
@@ -0,0 +1,40 @@
+package com.sitdh.master.eaa.main;
+
+public class B1Helicopter {
+	
+	public int paddleLength;
+	
+	private float weight;
+	
+	protected String blueprint;
+	
+	public B1Helicopter() {
+		
+	}
+
+	public int getPaddleLength() {
+		return paddleLength;
+	}
+
+	public void setPaddleLength(int paddleLength) {
+		this.paddleLength = paddleLength;
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+
+	public String getBlueprint() {
+		return blueprint;
+	}
+
+	public void setBlueprint(String blueprint) {
+		this.blueprint = blueprint;
+	}
+
+	
+}
