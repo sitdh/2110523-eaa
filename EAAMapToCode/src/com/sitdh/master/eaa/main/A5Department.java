35918c2b8d140c2cebe3644b61db940522fb9e5c
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-02-03 13:55:48 +0700

    Add department and professor class

---
+++ EAAMapToCode/src/com/sitdh/master/eaa/main/A5Department.java
@@ -0,0 +1,41 @@
+package com.sitdh.master.eaa.main;
+
+import java.util.Vector;
+
+public class A5Department {
+	
+	public String name;
+	
+	public String departmentDescription;
+	
+	public Vector<B5Professor> professors;
+	
+	public void addProfessor(B5Professor professor) {
+		professors.addElement(professor);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDepartmentDescription() {
+		return departmentDescription;
+	}
+
+	public void setDepartmentDescription(String departmentDescription) {
+		this.departmentDescription = departmentDescription;
+	}
+
+	public Vector<B5Professor> getProfessors() {
+		return professors;
+	}
+
+	public void setProfessors(Vector<B5Professor> professors) {
+		this.professors = professors;
+	}
+
+}
---
+++ EAAMapToCode/src/com/sitdh/master/eaa/main/B5Professor.java
@@ -0,0 +1,45 @@
+package com.sitdh.master.eaa.main;
+
+public class B5Professor {
+	
+	public String name;
+	
+	public String lastname;
+	
+	public String room;
+	
+	public String officehour;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getRoom() {
+		return room;
+	}
+
+	public void setRoom(String room) {
+		this.room = room;
+	}
+
+	public String getOfficehour() {
+		return officehour;
+	}
+
+	public void setOfficehour(String officehour) {
+		this.officehour = officehour;
+	}
+
+}
