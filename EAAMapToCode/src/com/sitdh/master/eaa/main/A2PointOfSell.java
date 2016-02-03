862fa2ba99621f26929bebb23472f40f5d3df71f
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-02-03 10:46:29 +0700

    1-1 Association

---
+++ EAAMapToCode/src/com/sitdh/master/eaa/main/A2PointOfSell.java
@@ -0,0 +1,44 @@
+package com.sitdh.master.eaa.main;
+
+public class A2PointOfSell {
+	
+	public float cashDeposit;
+	
+	private B2StaffProfile staff;
+	
+	protected String[] transaction;
+	
+	public A2PointOfSell() {
+		// TODO: Add constructor
+		staff = new B2StaffProfile();
+	}
+	
+	public void addTransaction() {
+		// TODO: Add sell transaction
+	}
+
+	public float getCashDeposit() {
+		return cashDeposit;
+	}
+
+	public void setCashDeposit(float cashDeposit) {
+		this.cashDeposit = cashDeposit;
+	}
+
+	public B2StaffProfile getStaff() {
+		return staff;
+	}
+
+	public void setStaff(B2StaffProfile staff) {
+		this.staff = staff;
+	}
+
+	public String[] getTransaction() {
+		return transaction;
+	}
+
+	public void setTransaction(String[] transaction) {
+		this.transaction = transaction;
+	}
+
+}
---
+++ EAAMapToCode/src/com/sitdh/master/eaa/main/B2StaffProfile.java
@@ -0,0 +1,35 @@
+package com.sitdh.master.eaa.main;
+
+public class B2StaffProfile {
+	
+	public String name;
+	
+	public String workingHour;
+	
+	protected float salary;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getWorkingHour() {
+		return workingHour;
+	}
+
+	public void setWorkingHour(String workingHour) {
+		this.workingHour = workingHour;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+}
