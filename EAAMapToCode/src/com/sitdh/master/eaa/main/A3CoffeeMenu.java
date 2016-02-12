f1bec13382c2e514ba96c52ef0c8f0d63f16f50f
Sitdhibong Laokok <sitdhibong@gmail.com>
2016-02-12 13:40:20 +0700

    CHANG SCENARIO AGAIN

--- EAAMapToCode/src/com/sitdh/master/eaa/main/A3CoffeeMenu.java
+++ EAAMapToCode/src/com/sitdh/master/eaa/main/A3CoffeeMenu.java
@@ -1,27 +1,28 @@
 package com.sitdh.master.eaa.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class A3CoffeeMenu {
 	
-	private String[] coffeeList;
+	private List<B3CoffeeDetail> coffeeList;
 	
 	public A3CoffeeMenu() {
-		coffeeList = new String[]{"Espresso,Small,2.40", "Espresso,Tall,3.00", "Hot Latte,Small,3.00", "Hot Latte,Tall, 3.50"};
+		coffeeList = new ArrayList<B3CoffeeDetail>(); // {"Espresso,Small,2.40", "Espresso,Tall,3.00", "Hot Latte,Small,3.00", "Hot Latte,Tall, 3.50"};
 	}
 	
-	public String[] getCoffeeList() {
+	public void setCoffeeList(List<B3CoffeeDetail> coffeeList) {
+		this.coffeeList = coffeeList;
+	}
+	
+	public List<B3CoffeeDetail> getCoffeeList() {
 		return coffeeList;
 	}
 	
 	public void displayMenu() {
-		B3CoffeeDetail espressoSmall = new B3CoffeeDetail("Espresso", "Small", 2.4f);
-		B3CoffeeDetail espressoTall = new B3CoffeeDetail("Espresso", "Tall", 3f);
-		B3CoffeeDetail latteSmall = new B3CoffeeDetail("Hot Latte", "Small", 3f);
-		B3CoffeeDetail latteTall = new B3CoffeeDetail("Hot Latte", "Tall", 3.5f);
-		
-		espressoSmall.print();
-		espressoTall.print();
-		latteSmall.print();
-		latteTall.print();
+		for(B3CoffeeDetail coffeeDetail : this.coffeeList) {
+			coffeeDetail.print();
+		}
 	}
 
 }
